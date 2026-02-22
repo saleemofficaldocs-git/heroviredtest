0e55ad9d443c53fac964571ac99113be04c62108
Saleem <saleemofficaldocs@gmail.com>
2026-02-22 09:08:18 +0530

    my first test commit

---
+++ odd-even.py
@@ -0,0 +1,10 @@
+#Q1. Write a Python program to check if a number is even or odd.
+
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is a Even Number".format(num))
+else:
+   print("{0} is an Odd Number".format(num))
+
+   
